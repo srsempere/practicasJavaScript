71ddfd3da9ae57c4bb072b238bd920c0cf5c60cc
Samu <samueljose.garcia@iesdonana.org>
2022-12-17 22:16:43 +0100

    Antes de cambiar el planteamiento de recogida de los coches

--- creacionObjetosAuto/script.js
+++ creacionObjetosAuto/script.js
@@ -1,6 +1,28 @@
-var boton = document.addEventListener("click", creaObjetos, false);
+// Función manejadora.
 
-function creaObjetos() {
-    var coches = document.getElementById("coches").value;
-    console.log(coches);
+function manejadorDeClick(event) {
+    if (event.target.name === "boton") { // Con esto me aseguro que el elemento que dispara el evento al clickar es el botón y no otro, como por ej. el input text.
+        var coches = document.getElementById("coches").value;
+        creaCoches(coches);
+    }
+
 }
+
+// Definición del objeto "coche".
+
+function coche(marca, ) {
+    // TODO: CREAR OBJETO COCHE.
+}
+
+
+
+// Función iterativa creadora de coches.
+function creaCoches(cars) {
+    // Aquí irían la creación automática de 5 objetos "cars" mediante un for.
+    // TODO: CREAR LA CREACIÓN DE COCHES DE FORMA ITERATIVA.
+}
+
+
+
+
+document.addEventListener("click", manejadorDeClick, false);
