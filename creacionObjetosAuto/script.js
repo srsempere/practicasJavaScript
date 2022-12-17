cfdc76949a70dd8d86685cf8b483a70d612dea2a
Samu <samueljose.garcia@iesdonana.org>
2022-12-17 21:42:23 +0100

    recgCrsKO

---
+++ creacionObjetosAuto/script.js
@@ -0,0 +1,6 @@
+var boton = document.addEventListener("click", creaObjetos, false);
+
+function creaObjetos() {
+    var coches = document.getElementById("coches").value;
+    console.log(coches);
+}
